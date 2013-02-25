3045bb733a5ee3ac5371440905acecd598243b7d
fsalum <fsalum@gmail.com>
2013-02-25 13:13:33 -0800

    update tests

--- tests/init.pp
+++ tests/init.pp
@@ -1,9 +1,6 @@
 node default {
 
   class { 'redis':
-    redis_package_ensure => 'present',
-    redis_service_ensure => 'running',
-    redis_service_enable => true,
     conf_port            => '6379',
     conf_bind            => '0.0.0.0',
   }
