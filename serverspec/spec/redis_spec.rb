51dc07ec4676a887e4765e6a786de131c9d07f8f
Tehmasp Chaudhri <tehmasp@gmail.com>
2014-01-13 23:31:16 -0700

    Added spec / serverspec tests

--- serverspec/spec/redis_spec.rb
+++ serverspec/spec/redis_spec.rb
@@ -1,6 +1,41 @@
 # For serverspec documentation - see http://serverspec.org/tutorial.html
 require_relative '../spec_helper'
 
-describe 'redis' do
-  it 'should do something'
+describe package('redis-server') do
+  it { should be_installed }
+end
+
+describe service('redis-server') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/etc/redis/redis.conf') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode '644' }
+end
+
+describe file('/etc/logrotate.d/redis-server') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode '644' }
+end
+
+describe file('/var/lib/redis/') do
+  it { should be_directory }
+  it { should be_owned_by 'redis' }
+  it { should be_grouped_into 'redis' }
+  it { should be_mode '755' }
+end
+
+describe command('redis-cli --version') do
+  it { should return_exit_status 0 }
+  it { should return_stdout /^redis-cli.*/ }
+end
+
+describe linux_kernel_parameter('vm.overcommit_memory') do 
+  its(:value) { should eq 1 }
 end
--- spec/classes/redis_spec.rb
+++ spec/classes/redis_spec.rb
@@ -2,5 +2,61 @@
 require_relative '../spec_helper'
 
 describe 'redis' do
-  it 'should do something'
+  let(:facts) { {:operatingsystem  => 'ubuntu'} }
+
+  it { should contain_class('redis::params') }
+
+  it do
+    should contain_package('redis').with(
+      'ensure'  =>  'present',
+      'name'    =>  'redis-server',
+    )
+  end
+
+  it do
+    should contain_service('redis').with(
+      'ensure'      =>  'running',
+      'name'        =>  'redis-server',
+      'enable'      =>  'true',
+      'hasrestart'  =>  'true',
+    )
+  end
+
+  it do
+    should contain_file('/etc/redis/redis.conf').with(
+      'path'  =>  '/etc/redis/redis.conf',
+      'owner' =>  'root',
+      'group' =>  'root',
+      'mode'  =>  '0644',
+    )
+  end
+
+  it do
+    should contain_file('/etc/logrotate.d/redis-server').with(
+      'path'  =>  '/etc/logrotate.d/redis-server',
+      'owner' =>  'root',
+      'group' =>  'root',
+      'mode'  =>  '0644',
+    )
+  end
+
+  it do
+    should contain_exec('/var/lib/redis/').with(
+      'path'    =>  '/bin:/usr/bin:/sbin:/usr/sbin',
+      'command' =>  'mkdir -p /var/lib/redis/',
+      'user'    =>  'root',
+      'group'   =>  'root',
+      'creates' =>  '/var/lib/redis/',
+    )
+  end
+
+  it do
+    should contain_file('/var/lib/redis/').with(
+      'ensure' =>  'directory',
+      'owner'  =>  'redis',
+      'group'  =>  'redis',
+      'mode'   =>  '0755',
+    )
+  end
+
 end
