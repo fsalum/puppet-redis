aa38d3e1b1e72b5d7ff26bb1206c0cdb95aa41ca
fsalum <fsalum@gmail.com>
2014-02-19 22:58:09 -0800

    Forge Release 0.0.11

--- tests/init.pp
+++ tests/init.pp
@@ -1,8 +1,14 @@
 node default {
 
-  class { 'redis':
-    conf_port            => '6379',
-    conf_bind            => '0.0.0.0',
+  package { 'epel-release':
+    ensure   => present,
+    source   => 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+    provider => rpm,
   }
 
-}
+  class { 'redis':
+    conf_port     => '6379',
+    conf_bind     => '0.0.0.0',
+    system_sysctl => true,
+  }
+
