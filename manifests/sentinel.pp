00671d2b6abbfa50b50888be735768345ac28b26
Victor Garcia <vgarcia@thousandeyes.com>
2014-07-18 13:38:21 -0700

    Bugfix, notify instead of subscribe

--- manifests/sentinel.pp
+++ manifests/sentinel.pp
@@ -93,12 +93,12 @@ class redis::sentinel (
   }
 
   file { $conf_sentinel:
-    path      => $conf_sentinel,
-    content   => template('redis/sentinel.conf.erb'),
-    owner     => redis,
-    group     => redis,
-    mode      => '0644',
-    subscribe => Service['sentinel'],
+    path    => $conf_sentinel,
+    content => template('redis/sentinel.conf.erb'),
+    owner   => redis,
+    group   => redis,
+    mode    => '0644',
+    notify  => Service['sentinel'],
   }
 
   file { $conf_logrotate:
